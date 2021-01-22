61ac77558dd1d30a16737a225c377a4f5e0930b1
Tafadzwa Ben <tbmukwepa@gmail.com>
2021-01-22 14:24:40 +0200

    Adding images

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store settings for Alien Invasion."""
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
