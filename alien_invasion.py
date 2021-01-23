ac628508b40781d6dcdb0e6d973876217c2e7138
Tafadzwa Ben <tbmukwepa@gmail.com>
2021-01-23 12:15:08 +0200

    Adding ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,17 +1,18 @@
 import sys
-
 import pygame
 from settings import Settings
-
+from ship import Ship
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
+    # the init method, pygame.init() function initializes background settings that pygame needs to word properly
     def __init__(self):
-
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
+        # pygame.display-> create a display window, on which well draw all game graphical elements
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -23,6 +24,7 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             #Make the most recently drawn screen visible.
             pygame.display.flip()
